d53a71a3845980f7007126bf96f7f2645b6cfdb8
iFLPX <phzinn636@gmail.com>
2026-09-11 07:48:27 -0300

    calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,33 @@
+function calcular() {
+    const numero1 = Number(document.getElementById("numero1").value);
+    const numero2 = Number(document.getElementById("numero2").value);
+    const operacao = document.getElementById("operacao").value;
+
+    let resultado;
+
+    switch (operacao) {
+        case "+":
+            resultado = numero1 + numero2;
+            break;
+
+        case "-":
+            resultado = numero1 - numero2;
+            break;
+
+        case "*":
+            resultado = numero1 * numero2;
+            break;
+
+        case "/":
+            if (numero2 === 0) {
+                alert("Não é possível dividir por zero!");
+                return;
+            }
+            resultado = numero1 / numero2;
+            break;
+    }
+
+    localStorage.setItem("resultado", resultado);
+
+    window.location.href = "resultado.html";
+}
